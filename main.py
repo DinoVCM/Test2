b8b92d1ad5095df33a868be8b56581ee698a20cd
DinoVCM <438964@aaps.k12.mi.us>
2025-01-15 10:08:49 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# first_person = input("Enter the name of the first person: ")
+# second_person = input("Enter the name of the second person: ")
+
+# if first_person > second_person:
+#   print(f"{first_person} is better than {second_person}")
+# else:
+#   print(f"{second_person} is better than {first_person}")
+
+eyes, nose, mouth = map(int, input().split())
+product = eyes * nose * mouth
+print(product)
